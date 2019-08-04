f91c0cfca48aee2c9b789545cb8f1337956e5358
rajram <rajram2000.dev@gmail.com>
2019-08-04 16:51:40 -0700

    Modify SHOWUPSTREAM to be verbose

--- .git-prompt/git-prompt-settings.sh
+++ .git-prompt/git-prompt-settings.sh
@@ -6,5 +6,5 @@ export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWCOLORHINTS=true
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
-export GIT_PS1_SHOWUPSTREAM="auto"
+export GIT_PS1_SHOWUPSTREAM="verbose name"
 export PROMPT_COMMAND='__git_ps1 "\h:\[\e[01;34m\]\w\[\e[00m\]" "\\\$ "'
