3cb5bc032046b0ca896686fb6b2129777d241899
rajram <rajram2000.dev@gmail.com>
2019-08-04 17:34:24 -0700

    Add newline to prompt

--- .git-prompt/git-prompt-settings.sh
+++ .git-prompt/git-prompt-settings.sh
@@ -7,4 +7,4 @@ export GIT_PS1_SHOWCOLORHINTS=true
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM="verbose name"
-export PROMPT_COMMAND='__git_ps1 "\h:\[\e[01;34m\]\w\[\e[00m\]" "\\\$ "'
+export PROMPT_COMMAND='__git_ps1 "\h:\[\e[01;34m\]\w\n\[\e[00m\]" "\\\$ "'
