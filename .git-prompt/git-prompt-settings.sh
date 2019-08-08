d2c74ec4d7e6cb8944024f01bc26ea7f1841945d
Raj Ramos <raj.ramos@navy.mil>
2019-08-08 07:54:22 -0700

    Update prompt style

--- .git-prompt/git-prompt-settings.sh
+++ .git-prompt/git-prompt-settings.sh
@@ -6,5 +6,5 @@ export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWCOLORHINTS=true
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
-export GIT_PS1_SHOWUPSTREAM="verbose name"
-export PROMPT_COMMAND='__git_ps1 "\h:\[\e[01;34m\]\w\n\[\e[00m\]" "\\\$ "'
+export GIT_PS1_SHOWUPSTREAM="verbose"
+export PROMPT_COMMAND='__git_ps1 "\[\e[01;33m\]\u@\h\[\e[00m\]:\[\e[01;32m\]\w\n\[\e[00m\]" "\\\$ "'
