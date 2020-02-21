574e7471fa51791e3965345eed59014ea241355e
rajram <rajram2000.dev@gmail.com>
2020-02-20 22:57:31 -0800

    Update settings

--- .git-prompt/git-prompt-settings.sh
+++ .git-prompt/git-prompt-settings.sh
@@ -3,8 +3,9 @@
 source ~/.git-prompt/git-prompt.sh
 
 export GIT_PS1_SHOWDIRTYSTATE=1
-export GIT_PS1_SHOWCOLORHINTS=true
+export GIT_PS1_SHOWCOLORHINTS=1
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
+export GIT_PS1_DESCRIBE_STYLE="contains"
 export GIT_PS1_SHOWUPSTREAM="verbose"
-export PROMPT_COMMAND='__git_ps1 "\[\e[01;33m\]\u@\h\[\e[00m\]:\[\e[01;32m\]\w\n\[\e[00m\]" "\\\$ "'
+export PROMPT_COMMAND='__git_ps1 "\[\e[01;32m\]\u@\h\[\e[00m\]:\[\e[01;33m\]\w\[\e[00m\]" "\n\\\$ "'
